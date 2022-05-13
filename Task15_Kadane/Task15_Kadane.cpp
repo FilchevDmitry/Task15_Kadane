88c82cfe460076bb656c11d3f3787c45b2e1a288
DmityFilchev <filchev_dmitry@mail.ru>
2022-05-13 13:43:06 +0300

    Добавьте файлы проекта.

---
+++ Task15_Kadane/Task15_Kadane.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<climits>
+using namespace std;
+int kadane(int a[], int size)
+{
+    int max = INT_MIN, sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum = sum + a[i];
+        if (max < sum)
+            max = sum;
+        if (sum < 0)
+            sum = 0;
+    }
+    return max;
+}
+int main()
+{
+    int a[] = { -2,1,-3,4,-1,2,1,-5,4 };
+    int n = sizeof(a) / sizeof(a[0]);
+    int max_sum = kadane(a, n);
+    cout << "Maximum contiguous sum is " << max_sum;
+    return 0;
+
+}
