5db567e4387d9549ad0c9e0a8872ca8cde609dbe
DmityFilchev <filchev_dmitry@mail.ru>
2022-05-13 15:26:16 +0300

    'ok'

--- Task15_Kadane/Task15_Kadane.cpp
+++ Task15_Kadane/Task15_Kadane.cpp
@@ -1,25 +1,30 @@
 ﻿#include<iostream>
 #include<climits>
+#include<vector>
 using namespace std;
-int kadane(int a[], int size)
-{
-    int max = INT_MIN, sum = 0;
-    for (int i = 0; i < size; i++)
-    {
-        sum = sum + a[i];
-        if (max < sum)
-            max = sum;
-        if (sum < 0)
-            sum = 0;
-    }
-    return max;
-}
 int main()
 {
-    int a[] = { -2,1,-3,4,-1,2,1,-5,4 };
-    int n = sizeof(a) / sizeof(a[0]);
-    int max_sum = kadane(a, n);
-    cout << "Maximum contiguous sum is " << max_sum;
+    vector<int>a = { -2,1,-3,4,-1,2,1,-8,4 };
+    int max = INT_MIN, sum = 0;
+    int start = 0,temp=0;
+    int end = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        sum += a[i];
+        if (max < sum)
+        {
+            max = sum;
+            end = i;
+            start = temp;
+           
+        }
+        if (sum < 0)
+        {
+            sum = 0;
+            temp = i+1;
+        } 
+    }
+    cout << max<<" "<<start <<" "<<end<< endl;
     return 0;
 
 }
